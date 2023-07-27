ea245b47ce30a60762d3853996f810b9fe3e1667
Kshitish <kshitishshetty@gmail.com>
2023-07-27 20:48:51 +0530

    Create Electricity_bill.c

---
+++ Electricity_bill.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+typedef struct
+{
+    char name[30];
+    char ID[10];
+    int  units;
+    double amount;
+} Info;
+
+int main(int argc,char **argv)
+{
+    
+    return 0;
+}
