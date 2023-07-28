33a555af29146565fe476b06552a3c53b2774bb8
Kshitish <kshitishshetty@gmail.com>
2023-07-28 20:27:29 +0530

    adding menu

--- Electricity_bill.c
+++ Electricity_bill.c
@@ -10,6 +10,22 @@ typedef struct
 
 int main(int argc,char **argv)
 {
+    int choice;
+    printf ("********************************************\n");
+    printf ("****************HANGMAN GAME****************\n");
+    printf ("********************************************\n");
+    while (1)
+    {
+        printf ("\n\t****MENU****\n");
+        printf ("PRESS 1 to INPUT DATA.\n");
+        printf ("PRESS 2 to GENERATE BILL.\n");
+        printf ("PRESS ANYKEY to EXIT.\n");
+        scanf ("%d", &choice);
+        switch(choice)
+        {
+            
+        }
+    }
     
     return 0;
 }
