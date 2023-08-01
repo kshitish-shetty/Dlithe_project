98000e5005b0d54922038807dfe17286ddc1bc55
Kshitish <kshitishshetty@gmail.com>
2023-08-01 22:03:00 +0530

    added struct header file

---
+++ fileinput.h
@@ -0,0 +1,13 @@
+
+typedef struct
+{
+    char name[30];
+    char ID[10];
+    int  units;
+    double amount;
+}Info;
+
+Info finput(char **filename)
+{
+    
+}
---
+++ structinfo.h
@@ -0,0 +1,7 @@
+typedef struct
+{
+    char name[30];
+    char ID[10];
+    int  units;
+    double amount;
+} Info;
