6eef855bf51f25b0190aed70fe1bea422942c460
Kshitish <kshitishshetty@gmail.com>
2023-08-02 20:46:43 +0530

    beautified menu

--- Electricity_bill.c
+++ Electricity_bill.c
@@ -19,68 +19,86 @@ int main(int argc,char **argv)
 	SetConsoleTitle("Electricity Bill Generator");
     Home:
 	system("cls");   // FOR CLEARING SCREEN
-	printf(" ---------------------------------------------------------------------------------\n");      
-	printf("|                                                                                 |\n");
-	printf("|  OOOOOO    OOOOOO  OOOOOO  OOOOOO  OOOOOO  OOOOOO  O      O  OOOOOOO   OOOOOO   |\n");
-	printf("|  O         O    O  O       O         O       O     O O    O  O         O        |\n");
-	printf("|  O  OOOOO  OOOOOO  OOOOO   OOOOO     O       O     O  O   O  O  OOOOO  OOOOOO   |\n");
-	printf("|  O    O    O  O    O       O         O       O     O   O  O  O    O         O   |\n");
-	printf("|  OOOOOO    O   O   OOOOOO  OOOOOO    O     OOOOOO  O    O O  OOOOOO    OOOOOO   |\n");
-	printf("|                                                                                 |\n");                       
-	printf(" ---------------------------------------------------------------------------------\n");
- 	printf("\t\t*************************************************\n");
-	printf("\t\t*                                               *\n");
-	printf("\t\t*       -----------------------------           *\n");
-	printf("\t\t*        WELCOME TO ELECTRICITY BILL            *\n");
-	printf("\t\t*                 GENERATOR                     *\n");
-	printf("\t\t*       -----------------------------           *\n");
-	printf("\t\t*                                               *\n");
-	printf("\t\t*                                               *\n");
-	printf("\t\t*                                               *\n");
-	printf("\t\t*                                               *\n");
-	printf("\t\t*       Brought To You By Team CP020            *\n");
-	printf("\t\t*************************************************\n\n\n");
+	printf("\t ---------------------------------------------------------------------------------\n");      
+	printf("\t|                                                                                 |\n");
+	printf("\t|  OOOOOO    OOOOOO  OOOOOO  OOOOOO  OOOOOO  OOOOOO  O      O  OOOOOOO   OOOOOO   |\n");
+	printf("\t|  O         O    O  O       O         O       O     O O    O  O         O        |\n");
+	printf("\t|  O  OOOOO  OOOOOO  OOOOO   OOOOO     O       O     O  O   O  O  OOOOO  OOOOOO   |\n");
+	printf("\t|  O    O    O  O    O       O         O       O     O   O  O  O    O         O   |\n");
+	printf("\t|  OOOOOO    O   O   OOOOOO  OOOOOO    O     OOOOOO  O    O O  OOOOOO    OOOOOO   |\n");
+	printf("\t|                                                                                 |\n");                       
+	printf("\t ---------------------------------------------------------------------------------\n");
+ 	printf("\t\t\t*************************************************\n");
+	printf("\t\t\t*                                               *\n");
+	printf("\t\t\t*         -----------------------------         *\n");
+	printf("\t\t\t*          WELCOME TO ELECTRICITY BILL          *\n");
+	printf("\t\t\t*                   GENERATOR                   *\n");
+	printf("\t\t\t*         -----------------------------         *\n");
+	printf("\t\t\t*                                               *\n");
+	printf("\t\t\t*                                               *\n");
+	printf("\t\t\t*                                               *\n");
+	printf("\t\t\t*                                               *\n");
+	printf("\t\t\t*         Brought To You By Team CP020          *\n");
+	printf("\t\t\t*************************************************\n\n\n\t");
 	for(i=0;i<80;i++)
 	printf("-");
-	printf("\nCurrent date and time : %s",ctime(&t));
+	printf("\n\t\t\tCurrent date and time : %s",ctime(&t));
+	printf("\t");
 	for(i=0;i<80;i++)
     printf("-");
-	printf(" \n Press any key to continue:");
-	getch();
-	while(1)     // INFINITE LOOP
+	printf("\n\t\t    Press 'A' For ADMIN Login.      Press ANYKEY For MAIN MENU.\n");
+	ch = getch();
+	if(ch=='a'||ch=='A')
 	{
 		system("cls");     
 		for(i=0;i<80;i++){		
+		    printf("-");
+		}
+		printf("\n   *****************************  |ADMIN LOGIN| ***************************** \n");
+		for(i=0;i<80;i++){
+			printf("-");
+		}
+		printf("\n");
+		getch();
+	}
+	else
+	while(1)     // INFINITE LOOP
+	{
+		system("cls"); 
+		printf("\t");    
+		for(i=0;i<80;i++){		
             printf("-");
         }
-		printf("\n   ******************************  |MAIN MENU|  ***************************** \n");
+		printf("\n \t  ******************************  |MAIN MENU|  ***************************** \n\t");
 		for(i=0;i<80;i++){
 	    	printf("-");
         }
 		printf("\n");
-		printf("\t\t *Please enter your choice for menu*:");
-		printf("\n\n");
-		printf(" \n Enter 1 -> Enter Customer Details");
-		printf("\n----------------------------------");
-		printf(" \n Enter 2 -> ");
-		printf("\n----------------------------------");
-		printf(" \n Enter 3 -> ");
-		printf("\n-----------------------------------");
-		printf(" \n Enter 4 -> ");
-		printf("\n-----------------------------------");
-		printf(" \n Enter 5 -> Return to Home Screen");
-		printf("\n-----------------------------------");
-		printf(" \n Enter 6 -> Exit");
-		printf("\n-----------------------------------");
+		printf("\t\t\t    *Please enter your choice for menu*:");
 		printf("\n");
+		printf("\n\t\t\t    ------------------------------------");
+		printf(" \n\t\t\t    | Enter 1 -> Enter Customer Details |");
+		printf("\n\t\t\t    ------------------------------------");
+		printf(" \n\t\t\t    | Enter 2 ->                        |");
+		printf("\n\t\t\t    ------------------------------------");
+		printf(" \n\t\t\t    | Enter 3 ->                        |");
+		printf("\n\t\t\t    -------------------------------------");
+		printf(" \n\t\t\t    | Enter 4 ->                        |");
+		printf("\n\t\t\t    -------------------------------------");
+		printf(" \n\t\t\t    | Enter 5 -> Return to Home Screen  |");
+		printf("\n\t\t\t    -------------------------------------");
+		printf(" \n\t\t\t    | Enter 6 -> Exit                   |");
+		printf("\n\t\t\t    -------------------------------------\n");
+		printf("\n\t");
 		for(i=0;i<80;i++){
 		    printf("-");
         }
-	    printf("\nCurrent date and time : %s",ctime(&t));
-        for(i=0;i<80;i++){
+	    printf("\n\t\t\tCurrent date and time : %s",ctime(&t));
+        printf("\t");
+		for(i=0;i<80;i++){
 		    printf("-");
         }
-        printf("\nSELECTION----> ");
+        printf("\n\tSELECTION----> ");
         choice = getch();
         switch(choice) // SWITCH STATEMENT
 		{	
@@ -99,17 +117,17 @@ int main(int argc,char **argv)
 			    goto Home;
 			case '6':
 				system("cls");
-				printf(" -------------------------------------------------------------------------\n");
-                printf("|                                                                         |\n");
-                printf("|      000000     0     0        0        0       0    0  0    000000     |\n");
-                printf("|        0        0     0       0 0       0 0     0    0 0     0          |\n");
-                printf("|        0        0000000      0 0 0      0  0    0    00      000000     |\n");
-                printf("|        0        0     0     0     0     0   0   0    0 0          0     |\n");
-                printf("|        0        0     0    0       0    0    0  0    0  0    000000     |\n");
-                printf("|                                                                         |\n");
-                printf(" -------------------------------------------------------------------------\n");
-				printf("\n\t\t    FOR USING OUR SERVICE !!!");
-				printf("\n\n\t  Press 'Y' To CONTINUE.    Press 'N' To CANCEL.");
+				printf("\t -------------------------------------------------------------------------\n");
+                printf("\t|                                                                         |\n");
+                printf("\t|      OOOOOO     O     O        O        O       O    O  O    OOOOOO     |\n");
+                printf("\t|        O        O     O       O O       O O     O    O O     O          |\n");
+                printf("\t|        O        OOOOOOO      O O O      O  O    O    OO      OOOOOO     |\n");
+                printf("\t|        O        O     O     O     O     O   O   O    O O          O     |\n");
+                printf("\t|        O        O     O    O       O    O    O  O    O  O    OOOOOO     |\n");
+                printf("\t|                                                                         |\n");
+                printf("\t -------------------------------------------------------------------------\n");
+				printf("\n\t\t\t    FOR USING OUR SERVICE !!!");
+				printf("\n\n\t\t  Press 'Y' To CONTINUE.    Press 'N' To CANCEL.");
 				ch=getch();
 				if(ch=='N'||ch=='n')
 				goto Home;
