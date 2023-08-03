9cda26fbea98bd96d96ba54b645006821a8071f8
Kshitish <kshitishshetty@gmail.com>
2023-08-03 21:27:46 +0530

    Final Interface

--- Electricity_bill.c
+++ Electricity_bill.c
@@ -22,8 +22,9 @@ void setConsoleSize(int width, int height) {
 int main(int argc,char **argv)
 {   
     char choice,ch;
+	char username[20],password[20];
     int i,width=99,height=30;
-	int pwCount=0;;
+	int pw_count=0,login_flag=0,admin_exit=0;
     time_t t;
 	time(&t);
 	SetConsoleTitle("Electricity Bill Generator");
@@ -52,21 +53,22 @@ int main(int argc,char **argv)
 	printf("\t\t\t*         Brought To You By Team CP020          *\n");
 	printf("\t\t\t*************************************************\n\n\t");
 	for(i=0;i<80;i++)
-	printf("-");
+		printf("-");
 	printf("\n\t\t\tCurrent date and time : %s",ctime(&t));
 	printf("\t");
 	for(i=0;i<80;i++)
-    printf("-");
-	if(pwCount>2){
+    	printf("-");
+	if(pw_count>2){
 		printf("\n\t\t                 Press ANYKEY For MAIN MENU.\n");	
 	}
 	else
 		printf("\n\t\t    Press 'A' For ADMIN Login.      Press ANYKEY For MAIN MENU.\n");
 	ch = getch();
-	if(!(ch=='a'||ch=='A')||pwCount>2)
+	if(!(ch=='a'||ch=='A')||pw_count>2)
 		goto MainMenu;
-	char username[20],password[20];
 	Admin:
+	if(login_flag)
+		goto AdminMenu;
 	system("cls");   
 	printf("\n\t");  
 	for(i=0;i<80;i++){		
@@ -114,21 +116,73 @@ int main(int argc,char **argv)
     }
     password[i] = '\0';
 	if(!strcmp(username,"ADMIN")&&!strcmp(password,"CP020")){
-		system("cls");
-		printf("WELCOME ADMIN");
-		getch();
+		do{	
+			login_flag=1;
+			AdminMenu:
+			system("cls");
+			printf("\n\t");    
+			for(i=0;i<80;i++){		
+			    printf("-");
+			}
+			printf("\n \t  ****************************  |ADMIN OPTIONS|  *************************** \n\t");
+			for(i=0;i<80;i++){
+				printf("-");
+			}
+			printf("\n\n\n");
+			printf("\t\t\t         *Please select your choice*");
+			printf("\n\n\n");
+			printf("\n\t\t\t   ---------------------------------------");
+			printf("\n\t\t\t   |  PRESS 1 -> Change Base Tariff      |");
+			printf("\n\t\t\t   ---------------------------------------");
+			printf("\n\t\t\t   |  PRESS 2 -> Change Payment Deadline |");
+			printf("\n\t\t\t   ---------------------------------------");
+			printf("\n\t\t\t   |  PRESS 3 -> Return to Home Screen   |");
+			printf("\n\t\t\t   ---------------------------------------");
+			printf("\n\t\t\t   |  PRESS 4 -> Exit                    |");
+			printf("\n\t\t\t   ---------------------------------------\n");
+			printf("\n\n\n\n\t");
+			for(i=0;i<80;i++){
+			    printf("-");
+			}
+			printf("\n\t\t\tCurrent date and time : %s",ctime(&t));
+			printf("\t");
+			for(i=0;i<80;i++){
+			    printf("-");
+			}
+			printf("\n");
+			choice = getch();
+			switch(choice){
+				case '1':
+
+				case '2':
+					system("cls");
+					printf("cumming soon. ;)");
+					getch();
+					break;
+				case '3': 
+					goto Home;
+				case '4':
+					admin_exit=1; 
+					goto Exit;
+				default:
+					system("cls");
+					printf("\n\n\n\n\t\t\t\t\tInvalid Input");
+					printf("\n\t\t\t\t  Press any key to continue");
+					getch();
+					break;
+			}
+		}while(1);
 	}
 	printf("\n\n\n\t\t\t\t    INCORRECT LOGIN CREDENTIALS");
-	printf("\n\n\n\t\t\t\t  %d Attempts Remain Before Lockout\n\n\t", 2-pwCount);
-	pwCount++;			
+	printf("\n\n\n\t\t\t\t  %d Attempts Remain Before Lockout\n\n\t", 2-pw_count);
+	pw_count++;			
 	for(i=0;i<80;i++)
-	printf("-");
+		printf("-");
 	printf("\n\t\t\tCurrent date and time : %s",ctime(&t));
 	printf("\t");
 	for(i=0;i<80;i++)
     	printf("-");
-
-	if(pwCount<3){
+	if(pw_count<3){
 		printf("\n\t\t    Press 'H' For Home.             Press ANYKEY For RETRY.\n");
 		ch = getch();
 		if(ch=='h'||ch=='H')
@@ -141,7 +195,6 @@ int main(int argc,char **argv)
 		getch();
 		goto Home;
 	}		
-
 	while(1)     
 	{	MainMenu:
 		system("cls"); 
@@ -153,23 +206,19 @@ int main(int argc,char **argv)
 		for(i=0;i<80;i++){
 	    	printf("-");
         }
-		printf("\n\n");
-		printf("\t\t\t     *Please enter your choice for menu*");
-		printf("\n\n");
-		printf("\n\t\t\t    ------------------------------------");
-		printf(" \n\t\t\t    | PRESS 1 -> Enter Customer Details |");
-		printf("\n\t\t\t    ------------------------------------");
-		printf(" \n\t\t\t    | PRESS 2 ->                        |");
-		printf("\n\t\t\t    ------------------------------------");
-		printf(" \n\t\t\t    | PRESS 3 ->                        |");
+		printf("\n\n\n");
+		printf("\t\t\t         *Please select your choice*");
+		printf("\n\n\n");
 		printf("\n\t\t\t    -------------------------------------");
-		printf(" \n\t\t\t    | PRESS 4 ->                        |");
+		printf("\n\t\t\t    | PRESS 1 -> Input Customer Data    |");
 		printf("\n\t\t\t    -------------------------------------");
-		printf(" \n\t\t\t    | PRESS 5 -> Return to Home Screen  |");
+		printf("\n\t\t\t    | PRESS 2 -> Generate Bill          |");
 		printf("\n\t\t\t    -------------------------------------");
-		printf(" \n\t\t\t    | PRESS 6 -> Exit                   |");
+		printf("\n\t\t\t    | PRESS 3 -> Return to Home Screen  |");
+		printf("\n\t\t\t    -------------------------------------");
+		printf("\n\t\t\t    | PRESS 4 -> Exit                   |");
 		printf("\n\t\t\t    -------------------------------------\n");
-		printf("\n\n\n\t");
+		printf("\n\n\n\n\t");
 		for(i=0;i<80;i++){
 		    printf("-");
         }
@@ -183,34 +232,36 @@ int main(int argc,char **argv)
         switch(choice) // SWITCH STATEMENT
 		{	
 			case '1':
-				
+			    
 			case '2':
-			    
-			case '3':
-			    
-			case '4':
 				system("cls");
 				printf("cumming soon. ;)");
 				getch();
 				break;
-			case '5':
+			case '3':
 			    goto Home;
-			case '6':
+			case '4':
+				Exit:
 				system("cls");
-				printf("\t -------------------------------------------------------------------------\n");
-                printf("\t|                                                                         |\n");
-                printf("\t|      OOOOOO     O     O        O        O       O    O  O    OOOOOO     |\n");
-                printf("\t|        O        O     O       O O       O O     O    O O     O          |\n");
-                printf("\t|        O        OOOOOOO      O O O      O  O    O    OO      OOOOOO     |\n");
-                printf("\t|        O        O     O     O     O     O   O   O    O O          O     |\n");
-                printf("\t|        O        O     O    O       O    O    O  O    O  O    OOOOOO     |\n");
-                printf("\t|                                                                         |\n");
-                printf("\t -------------------------------------------------------------------------\n");
-				printf("\n\t\t\t    FOR USING OUR SERVICE !!!");
-				printf("\n\n\t\t  Press 'Y' To CONTINUE.    Press 'N' To CANCEL.");
+				printf("\n\t -------------------------------------------------------------------------------\n");
+                printf("\t|                                                                               |\n");
+                printf("\t|         OOOOOO     O     O        O        O       O    O  O    OOOOOO        |\n");
+                printf("\t|           O        O     O       O O       O O     O    O O     O             |\n");
+                printf("\t|           O        OOOOOOO      O O O      O  O    O    OO      OOOOOO        |\n");
+                printf("\t|           O        O     O     O     O     O   O   O    O O          O        |\n");
+                printf("\t|           O        O     O    O       O    O    O  O    O  O    OOOOOO        |\n");
+                printf("\t|                                                                               |\n");
+                printf("\t -------------------------------------------------------------------------------\n");
+				printf("\n\t\t\t\t    FOR USING OUR SERVICE !!!");
+				printf("\n\n\t\t\t  Press 'Y' To CONTINUE.    Press 'N' To CANCEL.");
 				ch=getch();
-				if(ch=='N'||ch=='n')
+				if(ch=='N'||ch=='n'){
+					if(admin_exit==1){
+						admin_exit=0;
+						goto Admin;
+					}
 				goto MainMenu;
+				}
 				else if(ch=='Y'||ch=='y')
 				exit(0);
 			default:
