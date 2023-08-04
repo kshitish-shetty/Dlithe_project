e803a713e3c505b010771b2f19df103cf23171a6
Kshitish <kshitishshetty@gmail.com>
2023-08-04 21:07:29 +0530

    Merge branch 'fileinput'

--- struct.h
+++ struct.h
@@ -1,11 +1,8 @@
 #ifndef STRUCT_H
 #define STRUCT_H
 
-<<<<<<< Updated upstream
-=======
-#include<string.h>
 #include<stdlib.h>
->>>>>>> Stashed changes
+#include<string.h>
 
 typedef struct Info {
     char name[30];
@@ -16,5 +13,34 @@ typedef struct Info {
    struct Info *prev;
    struct Info *next;
 }Info;
+
+
+// Function to create new node
+Info* createNode(char *name, char *ID, int month, int  units, double amount) {
+    Info* newNode = (Info*)malloc(sizeof(Info));
+    strcpy(newNode->name,name);
+    strcpy(newNode->ID,ID);
+    newNode->month = month;
+    newNode->units = units;
+    newNode->amount = amount;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to insert node at the end of linked list
+void append(Info* head, char *name, char *ID, int month, int  units, double amount) {
+    Info* newNode = createNode(name, ID, month, units, amount);
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+    Info* current = head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    current->next = newNode;
+    newNode->prev = current;
+}
 
 #endif
