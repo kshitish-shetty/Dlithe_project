4caca48fb157196164a3d0b1ba1c13bf307dc9c1
dhanush182003 <dhanushyadav863@gmail.com>
2023-08-22 20:28:30 +0530

    Create bill_calculate.c

---
+++ bill_calculate.c
@@ -0,0 +1,8 @@
+//this is not a actual code ,just trying to know how to commit
+#include<stdio.h>
+
+int calcluate_bill(int units, float rate)
+{
+    float total_amt=rate*units;
+    return total_amt;
+}
