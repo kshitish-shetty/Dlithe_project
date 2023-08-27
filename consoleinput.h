b0f0a6e809a045c996ccc7b8d4ea425b93f4beef
Shriman-Jain <shrimantrial@gmail.com>
2023-08-27 16:54:32 +0530

    Create consoleinput.h

---
+++ consoleinput.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "interface.h"
+#include "struct.h"
+
+//Define a structure to hold customer information
+struct Customer{
+    char name[50]; //To hold customer Name
+    int units;     //To hold units 
+    float charges; //To hold charges
+}
+
+int main()
+{
+    header('INPUT')
+    return 0;
+}
