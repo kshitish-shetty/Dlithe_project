701e0716a5f1a42e3521954e52f727ee0c1a9b75
Shriman-Jain <shrimantrial@gmail.com>
2023-08-27 21:14:46 +0530

    Update consoleinput.h

--- consoleinput.h
+++ consoleinput.h
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include "interface.h"
 #include "struct.h"
-
-//Define a structure to hold customer information
-struct Customer{
-    char name[50]; //To hold customer Name
-    int units;     //To hold units 
-    float charges; //To hold charges
-}
-
-int main()
+                                              
+void main()
 {
-    header('INPUT')
-    return 0;
+    //Info*Details=(Info*)malloc(sizeof(Info));
+    header("INPUT");
+    struct Info Details;
+    printf("\n\n\n\n\n\t\t\t Customer Name :");//To get Customer Name
+    scanf("%s",Details.name);
+    printf("\n\n\n\n\n\t\t\t Customer ID   :");//To get Customer ID
+    scanf("%s",Details.ID);
+    printf("\n\n\n\n\n\t\t\t Units         :");//TO get Units
+    scanf("%d",Details.units);
+    printf("\n\n\n\n\n");
+    footer();
+
+
+
 }
