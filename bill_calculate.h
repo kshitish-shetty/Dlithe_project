22e4432ea3ea15115302f43d2a86b62b917d3a8a
dhanush182003 <dhanushyadav863@gmail.com>
2023-08-29 22:56:29 +0530

    updated

--- bill_calculate.h
+++ bill_calculate.h
@@ -1,12 +1,21 @@
-#include "struct.h"   // Including the header file "struct.h"
+#include "struct.h"  // Include necessary header file for Node and Info definitions
+#include "consoleinput.h"
+// Default rate per unit
+#define DEFAULT_RATE 0.59
 
-#define RATE 0.59      // Defining a constant RATE with the value 0.59
+// Global variable to store the rate per unit
+float RATE = DEFAULT_RATE;
 
-int bill_calculate(struct Info Details)
-{
-    // Function to calculate the bill based on the given Details
-    // Details is of type struct Info
+// Calculate bill based on the given rate
+float calculate_bill(Node* node) {
+    int units_consumed = node->data.units;
+    float total_bill = units_consumed * RATE;
+    return total_bill;
+}
 
-    int TOTAL = RATE * Details.units;   // Calculating the total bill
-    return TOTAL;                       // Returning the calculated total bill
+// Update the rate per unit using user input
+void update_rate() {
+    printf("Enter the new rate per unit: ");
+    scanf("%f", &RATE);
+    printf("Rate per unit updated to: %.2f\n", RATE);
 }
