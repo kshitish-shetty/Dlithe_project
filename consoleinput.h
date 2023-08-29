8d826d86c02825eb1da382e568f7d002de72b515
Shriman-Jain <shrimantrial@gmail.com>
2023-08-29 06:03:12 +0530

    Update consoleinput.h

--- consoleinput.h
+++ consoleinput.h
@@ -1,21 +1,59 @@
 #include <stdio.h>
+#include<stdlib.h>
 #include "interface.h"
 #include "struct.h"
-                                              
-void main()
-{
-    //Info*Details=(Info*)malloc(sizeof(Info));
-    header("INPUT");
-    struct Info Details;
-    printf("\n\n\n\n\n\t\t\t Customer Name :");//To get Customer Name
-    scanf("%s",Details.name);
-    printf("\n\n\n\n\n\t\t\t Customer ID   :");//To get Customer ID
-    scanf("%s",Details.ID);
-    printf("\n\n\n\n\n\t\t\t Units         :");//TO get Units
-    scanf("%d",Details.units);
-    printf("\n\n\n\n\n");
-    footer();
+  
 
+typedef struct Node {
+    Info data;
+    struct Node* next;
+} Node;
 
-
+// Function to create a new node with given data
+Node* createNode(Info newData) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = newData;
+    newNode->next = NULL;
+    return newNode;
 }
+
+// Function to insert a new node at the end of the linked list
+void insertNode(Node** head, Info newData) {
+    Node* newNode = createNode(newData);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    Node* current = *head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    current->next = newNode;
+}
+
+// Function to generate a bill for a given customer node
+void generateBill(Node* node) {
+    header("INPUT");
+    printf("\n\n\n\t\tCustomer Name  : %s\n\n\n", node->data.name);
+    printf("\n\n\n\t\tCustomer ID    : %s\n\n\n", node->data.ID);
+    printf("\n\n\n\t\tUnits Consumed : %d\n\n\n", node->data.units);
+    footer();
+}
+    
+   
+// Function to free memory used by the linked list
+void freeList(Node* head) {
+    while (head != NULL) {
+        Node* temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
+
+        
+
+
+
+
+
