e324b4369287306d0c58f8fa0ebb9959e0a86e8d
Kshitish <kshitishshetty@gmail.com>
2023-09-05 22:11:19 +0530

    Update Electricity_bill.c

--- Electricity_bill.c
+++ Electricity_bill.c
@@ -92,7 +92,7 @@ int main(int argc,char **argv)
 			    put = inputmenu(head);
 				switch(put){
 					case '1': 
-						GetUserData(&head);
+						getUserData(&head);
 						break;
 				}
 				break;
